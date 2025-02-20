abc6c9d111aed6130bf70b45ca442d24cd49e1e2
frangoJ <joaolucas142ajuda@gmail.com>
2025-02-20 11:52:26 -0300

    reiosse

--- trabalho que quis fazer/Assets/scrpts/PlayerController.cs
+++ trabalho que quis fazer/Assets/scrpts/PlayerController.cs
@@ -35,6 +35,8 @@ public class PlayerController : MonoBehaviour
                 addJumps--;
             }
         }
+        
+        textVida.text = vida.ToString();
     }
 
     void FixedUpdate()
